87bb6bf8c69a771299028cb3a288be2c83e70d33
jrsmth <jamesrichardsmith97@gmail.com>
2024-04-14 12:54:52 +0100

    Clean up tests

--- src/app/calendar/calendar_spec.py
+++ src/app/calendar/calendar_spec.py
@@ -1,19 +1,15 @@
-import json
-
 import pytest
-from flask import jsonify
 from liturgical_colour.liturgical import liturgical_colour
 from src.app.app import app
-from datetime import date, datetime
+from datetime import date
 
 
 class CalendarSpec:
     client = app.test_client()
-    today = date.today()
 
     @pytest.mark.parametrize("path,date", [
         ("2024-04-13", "2024-04-13"),
-        ("today", today)
+        ("today", date.today())
     ])
     def should_return_liturgical_information_for_a_given_date(self, path, date):
         """ Should return liturgical information for a given date """
